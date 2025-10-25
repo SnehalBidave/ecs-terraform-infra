ad604e7bc4076317c7ed6dbb053ede45401ee608
SnehalBidave <snehalkb7@gmail.com>
2025-10-25 13:12:53 +0530

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,38 @@
+resource "aws_lb" "main" {
+  name               = "${var.project_name}-alb"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  security_groups    = [aws_security_group.alb_sg.id]
+
+  tags = { Name = "${var.project_name}-alb" }
+}
+
+resource "aws_lb_target_group" "nginx_tg" {
+  name        = "${var.project_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"   # required for Fargate
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = { Name = "${var.project_name}-tg" }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+  }
+}
